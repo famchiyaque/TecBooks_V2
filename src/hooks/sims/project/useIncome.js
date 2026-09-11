74ff5bbc435d6d668e21295cd7c491bdc72388d0
Andre Agle <andrezala03@gmail.com>
2026-09-11 14:58:50 +0200

    feat: fix unit price calculations

--- src/hooks/sims/project/useIncome.js
+++ src/hooks/sims/project/useIncome.js
@@ -1,4 +1,7 @@
-import computeProductionCost from "@/sims/project-feasibility/income/computeProductionCosts";
+import computeProductionCost, {
+  toCostPerWorkOrder,
+} from "@/sims/project-feasibility/income/computeProductionCosts";
+import { sumAnnualSalariesByTitle } from "@/sims/project-feasibility/income/sumAnnualSalariesByTitle";
 import { cbmToCostTableInputs } from "@/sims/project-feasibility/costTable/cbmToCostTableInputs";
 
 function emptyIncome() {
@@ -16,40 +19,46 @@ function useIncome(project) {
     return emptyIncome()
   }
 
-  const productionCosts = computeProductionCost(project);
+  const inputs = cbmToCostTableInputs(project)
+  const production = inputs.production
+  const productionCosts = toCostPerWorkOrder(
+    computeProductionCost(project),
+    production,
+    sumAnnualSalariesByTitle(inputs.employees),
+    project.premises,
+  )
   const years = Object.keys(productionCosts.total)
     .map(Number)
-    .sort((a, b) => a - b);
-  const purchaseOrders =
-    cbmToCostTableInputs(project).production?.purchaseOrders ?? {};
+    .sort((a, b) => a - b)
+  const purchaseOrders = production?.purchaseOrders ?? {}
 
   const competitivaPrice = years.map((_, idx) => {
     return (
       project.bom.salePrice *
       Math.pow(1 + project.premises.nationalInflation[idx], idx)
-    );
-  });
+    )
+  })
 
   const sales = years.map((year, idx) => {
-    return (competitivaPrice[idx] ?? 0) * (purchaseOrders[year] ?? 0);
-  });
+    return (competitivaPrice[idx] ?? 0) * (purchaseOrders[year] ?? 0)
+  })
 
   const utilityCost = Object.values(productionCosts.total).reduce(
     (acc, yearTotal) => {
-      acc[10].push(yearTotal * 1.1);
-      acc[20].push(yearTotal * 1.2);
-      acc[30].push(yearTotal * 1.3);
-      return acc;
+      acc[10].push(yearTotal * 1.1)
+      acc[20].push(yearTotal * 1.2)
+      acc[30].push(yearTotal * 1.3)
+      return acc
     },
     { 10: [], 20: [], 30: [] },
-  );
+  )
 
   return {
     productionCosts,
     utilityCost,
     competitivaPrice,
     sales,
-  };
+  }
 }
 
-export default useIncome;
+export default useIncome
